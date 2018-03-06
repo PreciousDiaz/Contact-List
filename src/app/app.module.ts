3804890b853a8b539852808df0ecf7317bbdaa89
PreciousDiaz <elladiaz5@gmail.com>
2018-03-07 02:12:20 +0800

    Integrating Firestore into the angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule, AngularFirestore } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
 
+var firebaseConfig = {
+  apiKey: "AIzaSyA3S0VBldi81pJG4hdfc9EE3GQQ9lBBMVU",
+  authDomain: "contact-list-ca149.firebaseapp.com",
+  databaseURL: "https://contact-list-ca149.firebaseio.com",
+  projectId: "contact-list-ca149",
+  storageBucket: "contact-list-ca149.appspot.com",
+  messagingSenderId: "1060779367615"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
